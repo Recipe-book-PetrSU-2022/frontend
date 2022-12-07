ae0938243432e4c423c96b06818205efb426c91b
Vladislav <krysin18@mail.ru>
2022-12-07 07:53:05 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'recipe-book-petrsu-2022/frontend',
+  base: 'recipe-book-petrsu-2022/frontend/dist/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
